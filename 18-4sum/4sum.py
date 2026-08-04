6601ad55aa7933596e690086e079ab047bd6fd4b
dakshsharma29455 <dakshsharma2945@gmail.com>
2026-08-04 23:28:48 +0530

    Time: 992 ms (5.02%) | Memory: 19.4 MB (53.09%) - LeetSync

---
+++ 18-4sum/4sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
+        my_set = set()
+        for i in range(0,n):
+            for j in range(i+1,n):
+                you_set = set()
+                for k in range(j+1,n):
+                    fourth = target - (nums[i]+nums[j]+nums[k])
+                    if fourth in you_set:
+                        
+                        
+                        
+                    
+                        
+                        temp=[nums[i],nums[j],nums[k],fourth]
+                        temp.sort()
+                        my_set.add(tuple(temp))
+                    you_set.add(nums[k])    
+        return [list(ans) for ans in my_set]
