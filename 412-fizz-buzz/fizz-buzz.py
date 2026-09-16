73ed606c118a98b8e926583615600e84465597ff
dakshsharma29455 <dakshsharma2945@gmail.com>
2026-09-16 18:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (86.27%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -2,10 +2,11 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         output = []
         for i in range(1,n+1):
-            if i % 3 == 0:
+            if i % 15 == 0:
+                ans = "FizzBuzz"
+            elif i % 3 == 0:
                 ans = "Fizz"
-                if i % 5 == 0:
-                    ans += "Buzz"
+                
             elif i % 5 == 0:
                 ans = "Buzz"
             else:
