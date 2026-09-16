f4784ae9a3a2467c10c9a96b173b85a8cf415550
dakshsharma29455 <dakshsharma2945@gmail.com>
2026-09-16 19:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (9.38%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -3,16 +3,19 @@ class Solution:
         n = len(nums)
         low = 0
         high = n-1
+        
         while low <= high:
-            mid = (low + high)//2
+            mid = (low + high) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                
                 low = mid + 1
             else:
                 high = mid - 1
-        return -1        
+        return -1                
+
+
+                
                          
         
              
